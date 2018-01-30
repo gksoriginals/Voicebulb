ef54a572a732053ca50391445a2c847660b829e4
hadeeb <hadeebfarhan1@gmail.com>
2018-01-30 22:45:32 +0530

    Cross origin fix

--- app.py
+++ app.py
@@ -16,7 +16,51 @@ def get_file(filename):  # pragma: no cover
         return open(src).read()
     except IOError as exc:
         return str(exc)
+from datetime import timedelta
+from flask import make_response, request, current_app
+from functools import update_wrapper
 
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 @app.route('/',methods=['GET'])
 def fun1():
     content = get_file('html/index.html')
@@ -33,6 +77,7 @@ def fun3():
     return Response(content, mimetype="text/css")
 
 @app.route('/bulb',methods=['GET'])
+@crossdomain(origin='*')
 def send_recieve():
    control = {'status':None, 'text':None, 'blue_state':0, 'red_state':0}
    if request.method=='GET':
