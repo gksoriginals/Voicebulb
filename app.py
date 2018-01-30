9c0ef60c8974d4be71bad8525a7d8e34ca372988
hadeeb <hadeebfarhan1@gmail.com>
2018-01-30 22:54:40 +0530

    Fixed cross-origin decorator

--- app.py
+++ app.py
@@ -26,9 +26,9 @@ def crossdomain(origin=None, methods=None, headers=None,
                 automatic_options=True):
     if methods is not None:
         methods = ', '.join(sorted(x.upper() for x in methods))
-    if headers is not None and not isinstance(headers, basestring):
+    if headers is not None and not isinstance(headers, list):
         headers = ', '.join(x.upper() for x in headers)
-    if not isinstance(origin, basestring):
+    if not isinstance(origin, list):
         origin = ', '.join(origin)
     if isinstance(max_age, timedelta):
         max_age = max_age.total_seconds()
